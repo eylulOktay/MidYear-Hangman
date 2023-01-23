e81bdfaf019ce1bf163e690ef409ac370636c2a0
anaajesh <anaghaajesh2010@gmail.com>
2023-01-23 10:27:19 -0500

    merge changes

--- hangmanOpeningGui.py
+++ hangmanOpeningGui.py
@@ -15,16 +15,13 @@ class Screen_Opening(Frame):
 
         #title
         Label(self, text = "\n\n\n").grid(row = 0)
-<<<<<<< HEAD
         Label(self, text = "Hangman", font = "Georgia 45 bold", fg = "Hot Pink").grid(row = 3, column = 1)
         Label(self, text = "").grid(row = 4)
-=======
         Label(self, text = "Hangman!", font = "Georgia 45 bold", fg = "Blue").grid(row = 3, column = 1)
         Label(self, text = "").grid(row = 4)
         Label(self, text = "How to Play:\n\nTry to find the word by guessing letters!\nEvery incorrect letter will draw a part of the Hangman\nTry to find the word in 6 guesses before the full Hangman is drawn!", 
                     font = "Helvetica 15 italic", fg = "VioletRed3"
                     ).grid(row = 5, column = 0, columnspan = 3)
->>>>>>> f75b5ec39a6dcd51e0d9697a6a254daf522c2085
         Label(self, text = "").grid(row = 6)
         Button(self, text = "Press to Play", 
                      font = "Courier 12 bold", fg = "Maroon3", command = self.selected_play
