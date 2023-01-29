a9263251ad7d644643c7c02e86175974bb6d82a6
Eylul Oktay <gulzadeoktay@gmail.com>
2023-01-29 16:21:39 -0500

    openingGUI changes

--- hangmanOpeningGui.py
+++ hangmanOpeningGui.py
@@ -15,26 +15,29 @@ class Screen_Opening(Frame):
 
         #title
         Label(self, text = "\n\n\n").grid(row = 0)
-        title = Label(self, text = "Hangman!", font = "Georgia 45 bold", fg = "#F67280")
+        title = Label(self, text = "Hangman!", font = "Georgia 100 bold", fg = "#F67280")
         title.place(relx = 0.5, rely = 0.5, anchor='center')
-        title.grid(row = 0, column = 1)
+        title.grid(row = 0, column = 0)
         
         #Label(self, text = "").grid(row = 0, column = 1, columnspan = 2)
         #Label(self, text = "").grid(row = 0, column = 2)
        
         instruction = Label(self, text = "How to Play:\n\nTry to find the word by guessing letters!\nEvery incorrect letter will draw a part of the Hangman\nTry to find the word in 6 guesses before the full Hangman is drawn!", 
-                    font = "Helvetica 15 italic", fg = "#7286D3"
+                    font = "Helvetica 30 italic", fg = "#7286D3"
                     )
         instruction.place(relx = 1, rely = 1, anchor='center')
-        instruction.grid(row = 5, column = 1)
+        instruction.grid(row = 7, column = 0)
         
         Label(self, text = "").grid(row = 6)
         btn = Button(self, text = "Press to Play", 
-                     font = "Courier 12 bold", fg = "#ACDDDE", command = self.selected_play
+                     font = "Courier 25 bold", fg = "#ACDDDE", command = self.selected_play
                      )
         btn.place(relx = 1, rely = 1, anchor='center')
-        btn.grid(row = 7, column = 1)
+        btn.grid(row = 15, column = 0)
 
+        Label(self, text = "").grid(row = 6)
+        Label(self, text = "").grid(row = 10)
+        
     def selected_play(self):
         self.callback_on_play()
 
